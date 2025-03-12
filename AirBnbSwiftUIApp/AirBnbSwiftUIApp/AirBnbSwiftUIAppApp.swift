e703ee01af94ad69521b0ac8a8f83ef3b711ef33
Deepak sharma <ds284122@gmail.com>
2025-03-12 15:01:17 +0530

    initial commit

---
+++ AirBnbSwiftUIApp/AirBnbSwiftUIApp/AirBnbSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbSwiftUIAppApp.swift
+//  AirBnbSwiftUIApp
+//
+//  Created by Deepak Sharma on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirBnbSwiftUIApp/AirBnbSwiftUIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirBnbSwiftUIApp
+//
+//  Created by Deepak Sharma on 12/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
